e267c62ee1dc7fa2d6fa8e293bdc5fffb74d8603
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-02 08:49:18 -0300

    setting data for packages

--- set_data_coir.R
+++
@@ -1,8 +0,0 @@
-# Caminho das imagens dentro do pacote
-imgs <- list.files(path = "cropped-images/")
-
-# Criando o objeto
-data_coiR <- list(files = imgs)
-
-# Salvar no pacote
-usethis::use_data(data_coiR, overwrite = TRUE)
