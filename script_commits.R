cf325c21d91408717deb77f0f4adb6a6e4737e68
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-06 09:37:00 -0300

    Script to git commands

--- script_commits.R
+++ script_commits.R
@@ -4,15 +4,15 @@ library(gert)
 
 # Selecionar o arquivo ----
 
-gert::git_add("script_commits.R") 
+gert::git_add(list.files(pattern = "set")) |> as.data.frame() 
 
 # Commitar ----
 
-gert::git_commit("Script to commit")
+gert::git_commit("Script to set github repository")
 
 # Pushar ----
 
-gert::git_push()
+gert::git_push(remote = "origin", force = TRUE)
 
 ## Pullar ----
 
